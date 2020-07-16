7f3f1f7490230ee4158b34c7b26aa65d6a7f3593
Fearyao <965113581@qq.com>
2020-07-16 17:22:44 +0800

    vue-dev

--- vuelearn/v.js
+++ vuelearn/v.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Fearyao
  * @Date: 2020-06-15 12:58:22
- * @LastEditTime: 2020-06-29 10:17:20
+ * @LastEditTime: 2020-07-16 17:18:11
  * @LastEditors: Fearyao
  */
 
@@ -32,6 +32,7 @@ function observe(target) {
   }
   if (Array.isArray(target)) {
     target.__proto__ = arrayProto
+    return
   }
   for (let key in target) {
     defineReactive(target, key, target[key])
@@ -42,21 +43,20 @@ const oldArrayProperty = Array.prototype
 const arrayProto = Object.create(oldArrayProperty);
 ['push', 'pop'].forEach(method => {
   arrayProto[method] = function () {
-    oldArrayProperty[method].call(this, ...arguments)
     console.log('array change')
+    oldArrayProperty[method].call(this, ...arguments)
   }
 });
 
 const data = {
   name: 'fearyao',
   age: 26,
-  friend: {
-    l: 1
-  }
+  friend: []
 }
 
 observe(data)
 
 data.friend = [1, 2]
+console.log(data.friend)
 data.friend.push(3)
 
